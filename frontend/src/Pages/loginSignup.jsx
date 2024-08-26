620f0f64c4a0a020659c31f86600e64b85c223a4
Afaf <khadraouiafaf23@gmail.com>
2024-08-26 20:20:00 +0100

    Routes added

--- frontend/src/Pages/loginSignup.jsx
+++ frontend/src/Pages/loginSignup.jsx
@@ -1,1 +1,10 @@
 // use rafce after downalding the react js extension , it creates the functional component for you
+import React from 'react'
+
+const loginSignup = () => {
+  return (
+    <div>loginSignup</div>
+  )
+}
+
+export default loginSignup
