fa3f2729c9626a77afd58eeadb9f08ced6053248
Meliani Malak <meliani.malak16@gmail.com>
2024-09-05 18:58:34 +0100

    created route for cart

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,32 @@
 // src/App.js
 import React from 'react';
-import ProductList from './components/ProductList.js'; // Add .js extension
-import Cart from './components/Cart.js'; // Add .js extension
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import ProductList from './components/ProductList.js';
+import Cart from './components/Cart.js';
 
 function App() {
   return (
-    <div className="App">
-      <ProductList />
-      <Cart />
-    </div>
+    <Router>
+      <div className="App">
+        {/* Navigation Links */}
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Product List</Link>
+            </li>
+            <li>
+              <Link to="/cart">Cart</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* Define Routes */}
+        <Routes>
+          <Route path="/" element={<ProductList />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
